a2292e67ed98572edb33983fc01b5ae35aabfef1
GiPisano <pisanogiacomo9@gmail.com>
2024-02-06 18:44:05 +0100

    add html 'v-show' and js correction

--- js/main.js
+++ js/main.js
@@ -5,33 +5,28 @@ createApp({
         return {
             mails: [],
             loading: false,
-            numMail: 0,
         };
     },
 
     methods: {
         generateMail() {
-            const mailRequests = [];
 
             for (let i = 1; i <= 10; i++) {
                 const request = axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response) => {
-                        this.loading = true;
-                        this.mails.push(response.data.response);
-                        this.numMail++;
-
-                        if (this.numMail == 10) {
-                            this.loading = false;
-                        }
-                    });
-
-                mailRequests.push(request);
+                .then((response) => {
+                    this.loading = true;
+                    this.mails.push(response.data.response);
+                    if (this.mails.length == 10) {
+                        this.loading = false;
+                    }
+                });
+            
             }
-        }
-    },
+        },
 
-    created() {
-        this.generateMail();
+        created() {
+            this.generateMail();
+        },
     }
 }).mount('#app');
 
