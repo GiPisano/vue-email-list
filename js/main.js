094328d315a56c6db3a5bc81926bbb4e1c098ffe
GiPisano <pisanogiacomo9@gmail.com>
2024-02-06 18:45:34 +0100

    .

--- js/main.js
+++ js/main.js
@@ -13,20 +13,20 @@ createApp({
 
             for (let i = 1; i <= 10; i++) {
                 const request = axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    this.loading = true;
-                    this.mails.push(response.data.response);
-                    if (this.mails.length == 10) {
-                        this.loading = false;
-                    }
-                });
-            
-            }
-        },
+                    .then((response) => {
+                        this.loading = true;
+                        this.mails.push(response.data.response);
 
-        created() {
-            this.generateMail();
-        },
+                        if (this.mails.length == 10) {
+                            this.loading = false;
+                        }
+                    });
+            }
+        }
+    },
+
+    created() {
+        this.generateMail();
     }
 }).mount('#app');
 
